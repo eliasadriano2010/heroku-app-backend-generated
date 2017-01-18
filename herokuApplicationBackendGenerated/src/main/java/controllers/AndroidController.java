ed9d0e3b57e4d14299a66aa85cb7682da17463d9
eliasadriano <eliasadriano@192.168.0.103>
2017-01-18 05:29:14 -0200

    teste

--- herokuApplicationBackendGenerated/src/main/java/controllers/AndroidController.java
+++ herokuApplicationBackendGenerated/src/main/java/controllers/AndroidController.java
@@ -30,12 +30,13 @@ import dao.GenericDAOMongoDB;
 @Path("/android")
 public class AndroidController {
 
-// ---------------- attributes
-// ---------------------------------------------------------------
+	// ---------------- attributes
+	// ---------------------------------------------------------------
 
-private GenericDAOMongoDB<Android> genericDAOMongoDBAndroid;
+	private GenericDAOMongoDB<Android> genericDAOMongoDBAndroid;
 	private Result result;
 	private Validator validator;
+
 	// ---------------- Constructors
 	// --------------------------------------------------------------
 
@@ -46,7 +47,7 @@ private GenericDAOMongoDB<Android> genericDAOMongoDBAndroid;
 		// objectify needs register class
 		this.genericDAOMongoDBAndroid = new GenericDAOMongoDB<Android>(
 				Android.class);
-		
+
 		this.result = result;
 		this.validator = validator;
 	}
@@ -56,140 +57,148 @@ private GenericDAOMongoDB<Android> genericDAOMongoDBAndroid;
 	@Get
 	public void list() {
 		try {
-			result.use(ExtJSJson.class).from(genericDAOMongoDBAndroid.getEntities())
-					.success().serialize();
+			result.use(ExtJSJson.class)
+					.from(genericDAOMongoDBAndroid.getEntities()).success()
+					.serialize();
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
-	
-	// resources - generated from  services
-	// 0 - create | 1 - update | 2 - delete | 3 - retrieve 
+
+	// resources - generated from services
+	// 0 - create | 1 - update | 2 - delete | 3 - retrieve
 	// POST /android/saveOrUpdate => this method receives a POST request
-		// containing data in JSON and persists related entity
-		// accepted format:
-	
-		// var data = {
-		// android : {
-		// "id" : $scope.vm.id,
-		// "os" : $scope.vm.os != undefined ? $scope.vm.os:'',
-		// "ui" : $scope.vm.ui != undefined ? $scope.vm.ui : ''
-		// }
-		// }
-	
-		@Post
-		@Consumes("application/json")
-		public void create(Android android) {
-			AjaxReturn ajaxReturn = new AjaxReturn();
-			
-			// it would be better to use DTO desgin pattern to transfer data following - by now, let this way
-			
-			Android androidDTO = new Android();
-			if(android.getId()!=null){
-					androidDTO = genericDAOMongoDBAndroid.getEntityById(android.getId());
-			}
-			androidDTO.setOs(android.getOs());
-			androidDTO.setUi(android.getUi());
-		
-			try {
-				List<String> errors = validation(androidDTO);
-	
-				if (errors.isEmpty()) {
-					if (androidDTO.getId() == null) {
-						androidDTO.setId(ObjectId.get().toString());
-						genericDAOMongoDBAndroid.insertEntity(androidDTO);
-					} else {
-						genericDAOMongoDBAndroid.updateEntity(androidDTO, androidDTO.getId());
-					}
+	// containing data in JSON and persists related entity
+	// accepted format:
+
+	// var data = {
+	// android : {
+	// "id" : $scope.vm.id,
+	// "os" : $scope.vm.os != undefined ? $scope.vm.os:'',
+	// "ui" : $scope.vm.ui != undefined ? $scope.vm.ui : ''
+	// }
+	// }
+
+	@Post
+	@Consumes("application/json")
+	public void create(Android android) {
+		AjaxReturn ajaxReturn = new AjaxReturn();
+
+		// it would be better to use DTO desgin pattern to transfer data
+		// following - by now, let this way
+
+		Android androidDTO = new Android();
+		if (android.getId() != null) {
+			androidDTO = genericDAOMongoDBAndroid
+					.getEntityById(android.getId());
+		}
+		androidDTO.setOs(android.getOs());
+		androidDTO.setUi(android.getUi());
+
+		try {
+			List<String> errors = validation(androidDTO);
+
+			if (errors.isEmpty()) {
+				if (androidDTO.getId() == null) {
+					androidDTO.setId(ObjectId.get().toString());
+					genericDAOMongoDBAndroid.insertEntity(androidDTO);
 				} else {
-					ajaxReturn.setSucess(false);
-					ajaxReturn.addErrors(errors);
+					genericDAOMongoDBAndroid.updateEntity(androidDTO,
+							androidDTO.getId());
 				}
-			} catch (Exception ex) {
+			} else {
 				ajaxReturn.setSucess(false);
-				ajaxReturn.addErrors(genericDAOMongoDBAndroid.getErrors());
+				ajaxReturn.addErrors(errors);
 			}
-			result.use(ExtJSJson.class).from(ajaxReturn).serialize();
+		} catch (Exception ex) {
+			ajaxReturn.setSucess(false);
+			ajaxReturn.addErrors(genericDAOMongoDBAndroid.getErrors());
 		}
-	
-		// This private method receives an object performs validation and returns a
-		// list containing errors message
-		private List<String> validation(Android android) {
-			List<String> erros = new ArrayList<String>();
-			return erros;
-		}
+		result.use(ExtJSJson.class).from(ajaxReturn).serialize();
+	}
+
+	// This private method receives an object performs validation and returns a
+	// list containing errors message
+	private List<String> validation(Android android) {
+		List<String> erros = new ArrayList<String>();
+		return erros;
+	}
+
 	// POST /android/saveOrUpdate => this method receives a POST request
-		// containing data in JSON and persists related entity
-		// accepted format:
-	
-		// var data = {
-		// android : {
-		// "id" : $scope.vm.id,
-		// "os" : $scope.vm.os != undefined ? $scope.vm.os:'',
-		// "ui" : $scope.vm.ui != undefined ? $scope.vm.ui : ''
-		// }
-		// }
-	
-		@Post
-		@Consumes("application/json")
-		public void update(Android android) {
-			AjaxReturn ajaxReturn = new AjaxReturn();
-			try {
-				List<String> errors = validation(android);
-	
-				if (errors.isEmpty()) {
-					genericDAOMongoDBAndroid.insertEntity(android);
-					ajaxReturn.setSucess(errors.isEmpty());
-				} else {
-					ajaxReturn.setSucess(false);
-					ajaxReturn.addErrors(errors);
-				}
-			} catch (Exception ex) {
+	// containing data in JSON and persists related entity
+	// accepted format:
+
+	// var data = {
+	// android : {
+	// "id" : $scope.vm.id,
+	// "os" : $scope.vm.os != undefined ? $scope.vm.os:'',
+	// "ui" : $scope.vm.ui != undefined ? $scope.vm.ui : ''
+	// }
+	// }
+
+	@Post
+	@Consumes("application/json")
+	public void update(Android android) {
+		AjaxReturn ajaxReturn = new AjaxReturn();
+		try {
+			List<String> errors = validation(android);
+
+			if (errors.isEmpty()) {
+				genericDAOMongoDBAndroid.insertEntity(android);
+				ajaxReturn.setSucess(errors.isEmpty());
+			} else {
 				ajaxReturn.setSucess(false);
-				ajaxReturn.addErrors(genericDAOMongoDBAndroid.getErrors());
+				ajaxReturn.addErrors(errors);
 			}
-			result.use(ExtJSJson.class).from(ajaxReturn).serialize();
+		} catch (Exception ex) {
+			ajaxReturn.setSucess(false);
+			ajaxReturn.addErrors(genericDAOMongoDBAndroid.getErrors());
 		}
+		result.use(ExtJSJson.class).from(ajaxReturn).serialize();
+	}
+
 	@Get
-			@Post
-			@Consumes("application/json")
-			public void retrieve(String id) {
-				AjaxReturn ajaxReturn = new AjaxReturn();
-				try{
-					result.use(ExtJSJson.class).from(genericDAOMongoDBAndroid.getEntityById(id))
-						.serialize();
-				}catch(Exception ex){
-					ajaxReturn.addError(ex.getMessage());
-					result.use(ExtJSJson.class).from(ajaxReturn).serialize();
-				}
-			}
+	@Post
+	@Consumes("application/json")
+	public void retrieve(String id) {
+		AjaxReturn ajaxReturn = new AjaxReturn();
+		try {
+			result.use(ExtJSJson.class)
+					.from(genericDAOMongoDBAndroid.getEntityById(id))
+					.serialize();
+		} catch (Exception ex) {
+			ajaxReturn.addError(ex.getMessage());
+			result.use(ExtJSJson.class).from(ajaxReturn).serialize();
+		}
+	}
+
 	// POST /android/delete => this method receives a JSON request containing
-		// id as parameter, deleted related entity and
-		// answers if the entitiy was deleted successfully.
-		// accpted format:
-		// var data = {
-		// "id" : value
-		// }
-		@Post
-		@Consumes("application/json")
-		public void delete(String id) {
-			AjaxReturn ajaxReturn = new AjaxReturn();
-			try {
-				genericDAOMongoDBAndroid.deleteEntity(id);
-				if (genericDAOMongoDBAndroid.getErrors().isEmpty()) {
-					ajaxReturn.setSucess(true);
-					ajaxReturn.setAlert("Deleted sucessfully!");
-					result.use(ExtJSJson.class).from(ajaxReturn).serialize();
-				} else {
-					ajaxReturn.addErrors(genericDAOMongoDBAndroid.getErrors());
-					result.use(ExtJSJson.class).from(ajaxReturn).serialize();
-				}
-			} catch (Exception ex) {
-				ajaxReturn.addError(ex.getMessage());
+	// id as parameter, deleted related entity and
+	// answers if the entitiy was deleted successfully.
+	// accpted format:
+	// var data = {
+	// "id" : value
+	// }
+	@Post
+	@Consumes("application/json")
+	public void delete(String id) {
+		AjaxReturn ajaxReturn = new AjaxReturn();
+		try {
+			genericDAOMongoDBAndroid.deleteEntity(id);
+			if (genericDAOMongoDBAndroid.getErrors().isEmpty()) {
+				ajaxReturn.setSucess(true);
+				ajaxReturn.setAlert("Deleted sucessfully!");
+				result.use(ExtJSJson.class).from(ajaxReturn).serialize();
+			} else {
+				ajaxReturn.addErrors(genericDAOMongoDBAndroid.getErrors());
 				result.use(ExtJSJson.class).from(ajaxReturn).serialize();
 			}
-	
+		} catch (Exception ex) {
+			ajaxReturn.addError(ex.getMessage());
+			result.use(ExtJSJson.class).from(ajaxReturn).serialize();
 		}
-	
+
+	}
+
 	// create media methods - only generated if the entity has media
 }
--- herokuApplicationBackendGenerated/src/main/java/model/Android.java
+++ herokuApplicationBackendGenerated/src/main/java/model/Android.java
@@ -1,26 +1,31 @@
 package model;
 
-import org.codehaus.jackson.annotate.JsonProperty;
-
 import net.vz.mongodb.jackson.Id;
 import net.vz.mongodb.jackson.ObjectId;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.codehaus.jackson.annotate.JsonProperty;
 
 // dynamic imports
 //Generated Class
-public class Android 
- {
-		@ObjectId @Id
-		private String id;
-		private String os;
-		private String ui;
-	public Android(){
-		
+public class Android {
+	@ObjectId
+	@Id
+	private String id;
+	private String os;
+	private String ui;
+
+	public Android() {
+
 	}
-	
-	@JsonProperty("_id")	
+
+	public Android(String id, String os, String ui) {
+		super();
+		this.id = id;
+		this.os = os;
+		this.ui = ui;
+	}
+
+	@JsonProperty("_id")
 	public String getId() {
 		return id;
 	}
@@ -28,20 +33,21 @@ public class Android
 	public void setId(String id) {
 		this.id = id;
 	}
-		public String getOs( ) {
-			return this.os;
-		}
-		public void setOs(String os ) {
-			this.os = os;
-		} 
-		
-		public String getUi( ) {
-			return this.ui;
-		}
-		public void setUi(String ui ) {
-			this.ui = ui;
-		} 
-		
-		
+
+	public String getOs() {
+		return this.os;
 	}
 
+	public void setOs(String os) {
+		this.os = os;
+	}
+
+	public String getUi() {
+		return this.ui;
+	}
+
+	public void setUi(String ui) {
+		this.ui = ui;
+	}
+
+}
